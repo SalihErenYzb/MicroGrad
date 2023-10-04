4db5a9c1487465feab67079f15f97e7ef523b705
SalihErenYzb <algosalih@gmail.com>
2023-10-04 17:49:19 +0300

    Fixed inefficient code by adding topological sort, Cleaned the code, added tanh.

--- micrograd.py
+++ micrograd.py
@@ -1,57 +1,59 @@
+import math
 class Value( ):
-    def __init__(self, data,label=" "):
+    def __init__(self, data,_creator=tuple()):
         self.data = data
-        self.grad2 = 0
-        self.creator = None
-        self.op = None
+        self.grad = 0.0
+        self._creator = set(_creator)
+        self._backward = lambda: None
     def __add__(self,other):
-        tmp = Value(self.data+other.data)
-        tmp.creator= (self,other)
-        tmp.op = 1
-        return tmp
+        out = Value(self.data+other.data,_creator= (self,other))
+        def tmpbackward():
+            self.grad += out.grad
+            other.grad += out.grad
+
+
+        out._backward = tmpbackward
+        return out
+    def tanh(self):
+        x = self.data
+        out = Value((math.exp(2*x)-1)/(math.exp(2*x)+1),_creator=(self,))
+        def tmpbackward():
+            self.grad += 1-out.data**2
+        out._backward = tmpbackward
+        return out
     def __mul__(self,other):
-        tmp = Value(self.data*other.data)
-        tmp.creator= (self,other)
-        tmp.op = 0
-        return tmp
+        out = Value(self.data*other.data,_creator=(self,other))
+        def tmpbackward():
+            self.grad += other.data*out.grad
+            other.grad += self.data*out.grad
+        out._backward = tmpbackward        
+        return out
+    def backward(self):
+        self.grad = 1.0
+        visited = set()
+        tosort = []
+        def toposort(x):
+            if x not in visited:
+                visited.add(x)
+                for el in x._creator:
+                    toposort(el)
+                tosort.append(x)
+        toposort(self)
+        for i in reversed(tosort):
+            i._backward()
+        
+            
+            
 
     def __repr__(self):
         return f"{self.data}"
-    def grad(self,other=None,grad=1,highgrad=0):
-
-        if other==None:
-            self.grad2+=1*grad
-            tmp=1*grad
-        else:
-            self.grad2+=other.data*grad
-            tmp=other.data*grad
-
-        if self.creator==None:
-            
-            return
-        if self.op:
-            self.creator[0].grad(grad=tmp)
-            self.creator[1].grad(grad=tmp)
-
-        else:
-            self.creator[0].grad( self.creator[1],grad=tmp)
-            self.creator[1].grad( self.creator[0],grad=tmp)
-    
-
-a = Value(-2.0, label='a')
-b = Value(3.0, label='b')
-d = a * b    ; d.label = 'd'
-e = a + b    ; e.label = 'e'
-f = d * e    ; f.label = 'f'
-f.grad()
-print(a.grad2)
-print(b.grad2)
-print(d.grad2)
-print(e.grad2)
-
-print(f.grad2)
 
 
 
-
-
+a = Value(3)
+b = a+a
+c = b.tanh()
+c.backward()
+print(a.grad)
+print(b.grad)
+print(c.grad)
